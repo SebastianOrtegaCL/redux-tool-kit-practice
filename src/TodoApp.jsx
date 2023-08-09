15bab1e71d1b15c9fc58e3af236e05a7466d8750
sebastianCl <sebastian.ortega.fernandezz@gmail.com>
2023-08-09 19:42:01 -0400

    todo app api

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,27 +1,41 @@
-import { useGetTodosQuery } from "./store/api/index.js";
+import {useGetTodoQuery, useGetTodosQuery} from "./store/api/index.js";
+import {useState} from "react";
 
 export const TodoApp = () => {
-
-    const { data: todos = [], isLoading } = useGetTodosQuery();
+    // const { data: todos = [], isLoading } = useGetTodosQuery();
+    const [todoId, setTodoId] = useState(1)
+    const { data: todo, isLoading} = useGetTodoQuery(todoId);
+    const handleNextTodo = () => {
+        setTodoId(todoId + 1);
+    }
+    const handlePreviousTodo = () => {
+        if ( todoId === 1 ) return;
+        setTodoId( todoId - 1);
+    }
     return(
         <>
             <h1> Todos - RTK Query</h1>
             <hr />
             <h4>{ isLoading ? 'True': 'False' }</h4>
-            <pre> ... </pre>
-            <ul>
-                {
-                    todos.map( ({ title, id, completed}) => (
-                        <li key={id}>
-                            <strong> { completed ? 'Done !' : 'Pending' } </strong>
-                            { title }
-                        </li>
-                    ))
-                }
-            </ul>
-            <button>
+            <pre>{
+                JSON.stringify(todo)
+            }</pre>
+            <button onClick={ handlePreviousTodo }>
+                Previous todo
+            </button>
+            <button onClick={ handleNextTodo }>
                 Next todo
             </button>
+            {/*<ul>*/}
+            {/*    {*/}
+            {/*        todos.map( ({ title, id, completed}) => (*/}
+            {/*            <li key={id}>*/}
+            {/*                <strong> { completed ? 'Done !' : 'Pending' } </strong>*/}
+            {/*                { title }*/}
+            {/*            </li>*/}
+            {/*        ))*/}
+            {/*    }*/}
+            {/*</ul>*/}
         </>
     )
 }
